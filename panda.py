30f9e5b02e1051ff9eb592d35a7641a49f6b8d11
manojk-india <manokalyan2004@gmail.com>
2025-03-10 13:17:12 +0530

    output commit

--- panda.py
+++ panda.py
@@ -1,8 +1,4 @@
 import pandas as pd
 df = pd.read_csv("new_custom.csv")
-
-# Filter the DataFrame for issues where project is 'E-Commerce' and priority is 'Critical'
-filtered_df = df[(df['project'] == 'E-Commerce') & (df['priority'] == 'Critical')]
-
-# Save the filtered DataFrame to output.csv
-filtered_df.to_csv('output.csv', index=False)
+df_filtered = df[(df['project'] == 'E-Commerce') & (df['assignee'] == 'David')]
+df_filtered.to_csv('output.csv', index=False)
